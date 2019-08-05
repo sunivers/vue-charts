117921bd295247067b22876d17778ee065be4bcc
soyoung <soyoungp02@gmail.com>
2019-08-06 01:32:27 +0900

    컴포넌트 통신을 이용한 차트 컴포넌트 결합

--- src/components/BarChart.vue
+++ src/components/BarChart.vue
@@ -6,6 +6,7 @@
 
 <script>
 export default {
+    props: ['propsdata'],
     mounted() {
         // DOM에 접근할 때, 각 컴포넌트 내 태그에만 접근 가능한 ref 속성을 사용하는 것이 충돌 위험에서 안전하다.
         var ctx = this.$refs.barChart;
@@ -14,27 +15,7 @@ export default {
             type: 'bar',
             data: {
                 labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-                datasets: [{
-                    label: '# of Votes',
-                    data: [12, 19, 3, 5, 2, 3],
-                    backgroundColor: [
-                        'rgba(255, 99, 132, 0.2)',
-                        'rgba(54, 162, 235, 0.2)',
-                        'rgba(255, 206, 86, 0.2)',
-                        'rgba(75, 192, 192, 0.2)',
-                        'rgba(153, 102, 255, 0.2)',
-                        'rgba(255, 159, 64, 0.2)'
-                    ],
-                    borderColor: [
-                        'rgba(255, 99, 132, 1)',
-                        'rgba(54, 162, 235, 1)',
-                        'rgba(255, 206, 86, 1)',
-                        'rgba(75, 192, 192, 1)',
-                        'rgba(153, 102, 255, 1)',
-                        'rgba(255, 159, 64, 1)'
-                    ],
-                    borderWidth: 1
-                }]
+                datasets: this.propsdata,
             },
             options: {
                 scales: {
