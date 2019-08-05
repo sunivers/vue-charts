3abcd8e3c0e57a72dbae14309667fa2007e2f3f3
soyoung <soyoungp02@gmail.com>
2019-08-05 20:46:47 +0900

    라인 차트 추가

---
+++ src/components/LineChart.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+        <canvas id="lineChart" width="400" height="400"></canvas>
+    </div>
+</template>
+
+<script>
+import Chart from 'chart.js';
+
+export default {
+    mounted() {
+        var ctx = document.getElementById('lineChart').getContext('2d');
+        var chart = new Chart(ctx, {
+            // The type of chart we want to create
+            type: 'line',
+
+            // The data for our dataset
+            data: {
+                labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+                datasets: [{
+                    label: 'My First dataset',
+                    backgroundColor: 'rgb(255, 99, 132)',
+                    borderColor: 'rgb(255, 99, 132)',
+                    data: [0, 10, 5, 2, 20, 30, 45]
+                }]
+            },
+
+            // Configuration options go here
+            options: {}
+        });
+    }
+}
+</script>
+
+<style>
+
+</style>
